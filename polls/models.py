4ba01fb2c3cb5692cce49554b97a8e548921d17a
Adam Kawczynski <a.kawczynski@gmail.com>
2014-11-21 15:07:54 +0100

    ready for first deploy

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+
 from django.db import models
+from django.utils import timezone
+
 
 
 class Question(models.Model):
     question_text = models.CharField('the question', max_length=250)
     pub_date = models.DateTimeField('date published')
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.pub_date <= now
     
     def __unicode__(self):  # __str__ on Python 3
         return self.question_text
